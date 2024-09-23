33d0f8c9618be30ecee2498b184155bb6bb408b6
CodeNameHealer <swagatprasad3344@gmail.com>
2024-09-24 00:04:00 +0530

    initialised project

---
+++ src/sections/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='fixed top-0 left-0 z-50 w-full py-10'>
+            <div>
+                <a href="">
+                    <img src="/images/xora.svg" width={115} height={55} alt="logo" />
+                </a>
+            </div>
+        </header>
+    )
+}
+
+export default Header
