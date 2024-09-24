10456cff9437e311e26c2aafa9e3ac574768d674
CodeNameHealer <swagatprasad3344@gmail.com>
2024-09-24 12:10:00 +0530

    Working on button

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
+
+// sections
 import Header from './sections/Header'
+import Hero from './sections/Hero'
 
 const App = () => {
   return (
     <main className='overflow-hidden'>
       <Header />
+      <Hero />
     </main>
   )
 }
